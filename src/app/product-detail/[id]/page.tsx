b4632f292f0eb8c2817298ea4e7b84392cf1ef1c
Fred <frederik.koenig@posteo.de>
2024-12-09 16:32:33 +0100

    added gallery to product page

--- src/app/product-detail/[id]/page.tsx
+++ src/app/product-detail/[id]/page.tsx
@@ -5,6 +5,13 @@ import { Product } from "@/sanity/types";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Textarea } from "@/components/ui/textarea";
 import { AutosizeTextarea } from "@/components/ui/autosize-textarea";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 export default async function ProductDetail({
   params,
@@ -31,19 +38,46 @@ export default async function ProductDetail({
       }}
       className="pt-[11vw] md:pt-[7vw] flex flex-col justify-center items-center bg-cover"
     >
-      {/* // irgendwie kriegt er das bg-image noch nicht, das muss nochmal aktualisiert werden */}
       <div className="bg-zinc-400 bg-opacity-75 rounded-full pt-[1vw] m-[3vw] ">
         <h1 className="text-black font-bold text-3xl md:text-5xl lg:text-7xl px-[2vw]">
           {product.title}
         </h1>
       </div>
+      <div className="w-full">
+        <Carousel
+          opts={{
+            align: "center",
+            loop: true,
+          }}
+        >
+          <CarouselContent>
+            {product.gallery &&
+              product.gallery.map((image) => (
+                <CarouselItem key={image._key}>
+                  <img
+                    src={urlFor(image).url()}
+                    alt="Image"
+                    className="w-full object-cover rounded-2xl p-[3vw]"
+                  />
+                </CarouselItem>
+              ))}
+          </CarouselContent>
+          <div className="absolute top-1/2 left-2 flex items-center justify-center">
+            <CarouselPrevious className="relative left-[4vw] translate-x-0 hover:translate-x-0 hover:bg-zinc-200" />
+          </div>
+          <div className="absolute top-1/2 right-[4vw] flex items-center justify-center">
+            <CarouselNext className="relative right-0 translate-x-0 hover:translate-x-0 hover:bg-zinc-200" />
+          </div>
+        </Carousel>
+      </div>
 
+      {/* 
       {product.mainImage && (
         <img
           src={urlFor(product.mainImage).url()}
           className="p-[3vw] rounded-[7%]"
         ></img>
-      )}
+      )} */}
       <Tabs defaultValue="description" className="w-[90vw] mb-[5vw]">
         <TabsList>
           <TabsTrigger value="description">Beschreibung</TabsTrigger>
