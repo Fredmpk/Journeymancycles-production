f4e1dd520681ca1f34bd2e9a571d838990d84f14
Fred <frederik.koenig@posteo.de>
2025-05-21 11:06:22 +0200

    updated client.ts

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -1,8 +1,9 @@
 import { createClient } from "next-sanity";
+import { dataset, projectId } from "./env";
 
 export const client = createClient({
-  projectId: "m9asg8yh",
-  dataset: "production",
+  projectId,
+  dataset,
   apiVersion: "2024-11-01",
   useCdn: false,
 });
