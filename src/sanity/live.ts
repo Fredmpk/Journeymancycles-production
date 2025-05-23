bb3226caf6a300574f1c30a0dd3682e81e2a034d
Fred <frederik.koenig@posteo.de>
2025-05-23 11:33:24 +0200

    adjusted for live fetching

--- src/sanity/live.ts
+++ src/sanity/live.ts
@@ -1,6 +1,9 @@
 import { defineLive } from "next-sanity";
 import { client } from "@/sanity/client";
+import { token } from "./token";
 
 export const { sanityFetch, SanityLive } = defineLive({
   client: client.withConfig({ apiVersion: "2024-01-14" }),
+  serverToken: token,
+  browserToken: token,
 });
